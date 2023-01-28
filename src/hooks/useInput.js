89e8ea89b863a2698efe35ad851d52043a242df6
thdud <thgml1444@naver.com>
2023-01-28 10:38:59 +0900

    로그인 페이지 input, onChange-handler함수,  useState를 hook으로 만듦

---
+++ src/hooks/useInput.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const useInput = () => {
+  const [value, setValue] = useState("");
+  const onChangeHandler = (e) => {
+    setValue(e.target.value);
+  };
+  return [value, onChangeHandler];
+};
