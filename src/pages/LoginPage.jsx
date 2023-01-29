95815a1c0745db157d340da87e94b04b53417f20
thdud <thgml1444@naver.com>
2023-01-29 20:45:59 +0900

    로그인 회원가입 페이지 디자인 적용

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,45 @@
+import { useNavigate } from "react-router-dom";
 import { useInput } from "../hooks/useInput";
-import "../style/basicStyle.scss";
+import styles from "../style/LoginPageStyle.module.scss";
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
   const [email, onChangeEmail] = useInput();
   const [password, onChangePassword] = useInput();
 
   return (
-    <div>
-      <h2>ToonDa</h2>
-      <img />
-      <input type="text" value={email} onChange={onChangeEmail} />
-      <input type="password" vlaue={password} onChange={onChangePassword} />
-      <button>로그인하기</button>
-      <button>카카오톡으로 로그인하기</button>
-      <div className="SassComponent">
-        <div className="box red" />
-        <div className="box green" />
-        <div className="box yellow" />
+    <div className={styles.loginBox}>
+      <div className={styles.logoBox}>
+        <img src="/img/test.jpg" />
+        <h2>ToonDa</h2>
+      </div>
+      <div className={styles.inputBox}>
+        <div className={styles.inputList}>
+          <p>이메일</p>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChangeEmail}
+            placeholder="이메일을 입력하세요"
+          />
+        </div>
+        <div className={styles.inputList}>
+          <p>비밀번호</p>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+      </div>
+      <div className={styles.buttonBox}>
+        <button>로그인하기</button>
+        <span>또는</span>
+        <button>카카오톡으로 로그인하기</button>
+        <span onClick={() => navigate("/signup")}>회원가입하러 가기</span>
       </div>
     </div>
   );
