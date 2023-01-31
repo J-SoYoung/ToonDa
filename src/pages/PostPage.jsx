a83201f919d383ea8ecdfcc9fd132ca43066a53b
thdud <thgml1444@naver.com>
2023-01-31 11:15:05 +0900

    상세페이지 컴포넌트 생성 및 디자인 시작

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
+import { loadItem } from "../common/storage";
 
 export const PostPage = () => {
+  const keyowrd = loadItem("postKeyword");
+  const [postKeyword, setPostKeyword] = useState(keyowrd);
   return (
-    <div>PostPage</div>
-  )
-}
+    <>
+      {keyowrd === "ToonDiary" ? (
+        <div>새 툰 다이어리 만들기 컴포넌트</div>
+      ) : (
+        <div>새 툰 만들기 컴포넌트</div>
+      )}
+    </>
+  );
+};
