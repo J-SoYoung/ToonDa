1e6176d0550465049824d284120b6618552d83ca
thdud <thgml1444@naver.com>
2023-02-01 10:43:38 +0900

    [ADD] 상세페이지 디자인 작업 진행중입니다. 상세 첫 화면인 툰 다이어리 뷰 마무리, 푸터 수정중

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -5,17 +5,27 @@ import { FooterBar } from "../components/navBar/FooterBar";
 
 export const DetailPage = () => {
   return (
-    <div className={styles.detailBox}>
-      <div className={styles.detailMain}>
-        <img src="/img/ee.jpg" />
-        <div className={styles.diaryInfoBox}>
-          <img src="/img/user.jpg" />
-          <div>
-            <p>하루의 생각</p>
+    <div className={styles.detail}>
+      <div className={styles.detailBox}>
+        <div className={styles.detailMain}>
+          <div className={styles.imgBox}>
+            <img src="/img/ee.jpg" />
+          </div>
+          <div className={styles.diaryInfoBox}>
+            <div>
+              <img src="/img/user.jpg" />
+              <div>
+                <p>하루의 생각</p>
+                <p>우리는 매일 생각하며 살아간다</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.diaryTitle}>
             <p>우리는 매일 생각하며 살아간다</p>
+            <p>툰 다이어리</p>
           </div>
         </div>
-        <div className={styles.diaryTitle}>제목입니다 제목입니다</div>
+
       </div>
       <FooterBar />
     </div>
