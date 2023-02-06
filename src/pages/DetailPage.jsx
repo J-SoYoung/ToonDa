69fb3715b4afe3374123d09f531549a78068694c
thdud <thgml1444@naver.com>
2023-02-06 10:50:35 +0900

    [ADD] 상세페이지 하단 footer를 다이어리 내부의 중간 네비게이션으로 바꿨습니다. - 무한스크롤 기능을 사용할 것이기 때문에 기존의 스타일을 변경하였습니다. - 5일 저녁에 작업했던 부분인데, 커밋 취소를 하면서 함께 사라져 재작업 하였습니다.

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "../style/detailPageStyle.module.scss";
 
 import { FooterBar } from "../components/navBar/FooterBar";
+import { MiddleNavBar } from "../components/navBar/MiddleNavBar";
 
 export const DetailPage = () => {
   return (
@@ -11,6 +12,7 @@ export const DetailPage = () => {
           <div className={styles.imgBox}>
             <img src="/img/ee.jpg" />
           </div>
+
           <div className={styles.diaryInfoBox}>
             <div>
               <img src="/img/user.jpg" />
@@ -20,14 +22,50 @@ export const DetailPage = () => {
               </div>
             </div>
           </div>
+          <MiddleNavBar />
           <div className={styles.diaryTitle}>
             <p>우리는 매일 생각하며 살아간다</p>
             <p>툰 다이어리</p>
           </div>
         </div>
 
+        <div className={styles.detailList}>
+          <div className={styles.ListSubTitle}>
+            <p>2023년 2월 6일</p>
+            <p>부제목은 10글자로제한</p>
+          </div>
+          <div className={styles.imgBox}>
+            <img src="/img/ee.jpg" />
+          </div>
+          <MiddleNavBar />
+          <div className={styles.diaryTitle}>
+            <span>
+              우리는 매일 생각하며 살아간다 동해물과 백두산이 마르고 닳도록
+              하나님이 보우하사 우리나라만세 무궁화 삼천리 화려강산 대한사람
+              대한으로 길이 보전하세 우리나라만세 무궁화 삼천리 화려
+            </span>
+          </div>
+        </div>
+        
+        <div className={styles.detailList}>
+          <div className={styles.ListSubTitle}>
+            <p>2023년 2월 6일</p>
+            <p>부제목은 10글자로제한</p>
+          </div>
+          <div className={styles.imgBox}>
+            <img src="/img/ee.jpg" />
+          </div>
+          <MiddleNavBar />
+          <div className={styles.diaryTitle}>
+            <span>
+              우리는 매일 생각하며 살아간다 동해물과 백두산이 마르고 닳도록
+              하나님이 보우하사 우리나라만세 무궁화 삼천리 화려강산 대한사람
+              대한으로 길이 보전하세 우리나라만세 무궁화 삼천리 화려
+            </span>
+          </div>
+        </div>
+
       </div>
-      <FooterBar />
     </div>
   );
 };
