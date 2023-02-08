2f3d7b862baa867c6059658830d17ad7225677fd
thdud <thgml1444@naver.com>
2023-02-08 09:58:24 +0900

    [ADD] 구독자 리스트 페이지, 공감글 페이지 생성 / 디자인을 하였습니다

--- src/pages/SubscribePage.jsx
+++ src/pages/SubscribePage.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { SubNavBar } from "../components/navBar/SubNavBar";
+import styles from "../style/subscribePageStyle.module.scss";
 
 export const SubscribePage = () => {
-  return <div>SubscribePage</div>;
+  return (
+    <>
+      <SubNavBar children="구독중" />
+      <div className={styles.subscribeBox}>
+        <div>
+          <img src="/img/user.jpg" />
+          <p>툰다툰다</p> 
+        </div>
+        <div>
+          <img src="/img/user.jpg" />
+          <p>툰다툰다</p>
+        </div>
+        <div>
+          <img src="/img/user.jpg" />
+          <p>툰다툰다</p>
+        </div>
+      </div>
+    </>
+  );
 };
