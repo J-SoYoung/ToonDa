5228ebc9d442cbd3c28a5524ca466e2f61bff244
thdud <thgml1444@naver.com>
2023-02-11 23:42:13 +0900

    [ADD] 검색페이지의 네비게이션 바를 구현하라

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { SubNavBar } from "../components/navBar/SubNavBar";
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const handleSearchBar = () => {
+    console.log("검색바 출력");
+  };
+  return (
+    <>
+      <SubNavBar children={"검색"} search={true} handleFunc={handleSearchBar} />
+      <div>SearchPage</div>
+    </>
+  );
 };
