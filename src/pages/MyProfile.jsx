92af1c8f992b63ab439949db06561233c6b519f2
thdud <thgml1444@naver.com>
2023-02-14 10:19:04 +0900

    [ADD] myprofile-page 디자인을 하라

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,37 +1,57 @@
 import React, { useRef, useState } from "react";
+import { SubNavBar } from "../components/navBar/SubNavBar";
+import styles from "../styles/myprofilePage.module.scss";
+import { ReactComponent as Icon_WhitePencil } from "../assets/white_pencil.svg";
+import { useInput } from "../hooks/useInput";
 
 export const MyProfile = () => {
-  const [nickname, setNickname] = useState("");
-  const nameInput = useRef();
+  const [nickname, onChangeNickname] = useInput();
+  const [diaryIntro, onChangeDiaryIntro] = useInput();
 
-  const count = useRef(0);
-  
-  const handleClick = () => {
-    count.current = count.current + 1;
-    console.log(count.current);
+  const handleProfileEdit = () => {
+    console.log("v프로필수정");
   };
 
-  const onReset = () => {
-    setNickname("");
-    nameInput.current.focus();
+  const handleImageEdit = () => {
+    alert("모달: 기본이미지로 변경, 이미지불러오기, 취소");
   };
-
   return (
-    <div>
-      <div>counter: {count.current}</div>
-      <button onClick={handleClick}>+</button>
-      <hr />
-      <input
-        name="nickname"
-        placeholder="nickname"
-        onChange={(e) => {
-          setNickname(e.target.value);
-        }}
-        value={nickname}
-        ref={nameInput}
+    <>
+      <SubNavBar
+        children="프로필수정"
+        checkbox={true}
+        handleFunc={handleProfileEdit}
       />
-      <button onClick={onReset}>초기화</button>
-      <p>값:{nickname}</p>
-    </div>
+      <div className={styles.myprofileBox}>
+        <div className={styles.imgBox}>
+          <img src="/img/user.jpg" />
+          <p onClick={handleImageEdit}>
+            <Icon_WhitePencil className={styles.icon} />
+          </p>
+        </div>
+        <div className={styles.textBox}>
+          <div>
+            <p>닉네임</p>
+            <input
+              placeholder="defaultValue"
+              value={nickname}
+              maxLength="10"
+              onChange={onChangeNickname}
+            />
+            <span>{nickname.length}/10</span>
+          </div>
+          <div>
+            <p>소&nbsp;&nbsp;개</p>
+            <input
+              placeholder="나를 소개해주세요"
+              value={diaryIntro}
+              maxLength="40"
+              onChange={onChangeDiaryIntro}
+            />
+            <span>{diaryIntro.length}/40</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
