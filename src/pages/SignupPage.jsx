67f4a9a69fda758e7e76866ea9b4cdc8b481ea23
thdud <thgml1444@naver.com>
2023-02-26 11:36:21 +0900

    [FIX] 로그인, 회원가입 페이지 서버와 통신하라. - axios를 통해 서버와 통신하였다. interceptor를 사용해 서버와 통신 중에 토큰을 넣어 보낼 수 있도록 작성하였다. - 버그 : 네트워크탭에서 보이는 토큰을 가져올 수 없다. 왜지??

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { emailRegex, passwordRegex } from "../common/regEx";
-import { DeveloperInfo } from "../components/DeveloperInfo";
-import { useInput } from "../hooks/useInput";
-import styles from "../styles/loginPageStyle.module.scss";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { emailRegex, passwordRegex } from '../common/regEx';
+import { DeveloperInfo } from '../components/DeveloperInfo';
+import { useInput } from '../hooks/useInput';
+import { emailCheckApi, nicknameCheckApi } from '../service/api';
+
+import styles from '../styles/loginPageStyle.module.scss';
+import { Icon_G_EyeOpen, Icon_G_EyeClose } from '../assets/index';
 
 export const SignupPage = () => {
   const navigate = useNavigate();
+  const [view, setView] = useState(false);
 
   const [email, onChangeEmail] = useInput();
   const [username, onChangeUsername] = useInput();
@@ -16,49 +20,56 @@ export const SignupPage = () => {
   // 중복확인 체크, 비밀번호 재확인
   const [emailDoubleCheck, setEmailDoubleCheck] = useState(true);
   const [usernameDoubleCheck, setUsernameDoubleCheck] = useState(false);
-  const [passwordDoubleCheck, setPasswordDoubleCheck] = useState("");
+  const [passwordDoubleCheck, setPasswordDoubleCheck] = useState('');
 
   // 정규식 확인
   const [emailRegExCheck, setEmailRegExCheck] = useState(true);
-  const [passwordRegExCheck, setPasswordRegExCheck] = useState("");
+  const [passwordRegExCheck, setPasswordRegExCheck] = useState('');
   const [usernameCheck, setUsernameCheck] = useState(true);
 
   const handleClickSignup = () => {
-    //
+    // console.log
   };
 
   // 이메일, 닉네임 중복확인 + 정규식도 같이 확인
   const handleEmailDoubleCheck = () => {
     // 중복확인 API
-    if (email === "") {
-      alert("이메일 형식을 확인해주세요");
+    if (email === '') {
       setEmailDoubleCheck(false);
       return;
     }
     if (!emailRegex.test(email)) {
-      alert("이메일 형식을 확인해주세요");
       setEmailDoubleCheck(false);
       setEmailRegExCheck(false);
       return;
     }
-    setEmailDoubleCheck(true);
-    alert("사용가능한 이메일입니다");
+    emailCheckApi(email).then((res) => {
+      if (res.data.success === false) {
+        // setEmailDoubleCheck(true);
+      } else {
+        alert('중복된 이메일입니다');
+        // setEmailDoubleCheck(false);
+      }
+      console.log(res);
+    });
+    // setEmailDoubleCheck(true);
   };
 
   const handleUsernameDoubleCheck = () => {
     console.log(username);
     if (username.length < 5 || username.length > 10) {
       setUsernameCheck(false);
-      alert("닉네임을 확인해주세요");
+      alert('닉네임을 확인해주세요');
       return;
     }
     setUsernameCheck(true);
     setUsernameDoubleCheck(true);
-    alert("사용가능한 닉네임입니다");
+    nicknameCheckApi(username);
+    // alert("사용가능한 닉네임입니다");
   };
 
   const goLogin = () => {
-    navigate("/");
+    navigate('/');
     //
   };
 
@@ -76,7 +87,7 @@ export const SignupPage = () => {
             <div className={styles.subCheck}>
               <input
                 type="email"
-                value={email || ""}
+                value={email || ''}
                 onChange={onChangeEmail}
                 placeholder="이메일 주소를 입력하세요"
                 // readOnly={emailDoubleCheck ? true : false}
@@ -84,9 +95,7 @@ export const SignupPage = () => {
               <button onClick={handleEmailDoubleCheck}>중복확인</button>
             </div>
             {!emailDoubleCheck ? (
-              <label
-                className={emailDoubleCheck ? styles.default : styles.warning}
-              >
+              <label className={emailDoubleCheck ? null : styles.warning}>
                 이메일 형식을 확인하세요
               </label>
             ) : null}
@@ -97,7 +106,7 @@ export const SignupPage = () => {
             <div className={styles.subCheck}>
               <input
                 type="text"
-                value={username || ""}
+                value={username || ''}
                 onChange={onChangeUsername}
                 placeholder="닉네임을 작성해주세요"
                 // readOnly={usernameDoubleCheck ? true :  false}
@@ -113,33 +122,49 @@ export const SignupPage = () => {
             <p>비밀번호 </p>
             <input
               type="password"
-              value={password || ""}
+              value={password || ''}
               onChange={onChangePassword}
               placeholder="비밀번호를 작성해 주세요"
             />
-            <label warning={"default"}>
+            <label warning={'default'}>
               {/* <label warning={passwordRegExCheck ? false : true}> */}
               영어 대소문자, 숫자, 특수문자를 포함한 8-16자를 입력하세요
             </label>
+            <button
+              className={styles.signupBtn}
+              onClick={() => {
+                setView(!view);
+              }}
+            >
+              <img src={view ? Icon_G_EyeOpen : Icon_G_EyeClose} />
+            </button>
           </div>
 
           <div className={styles.inputList}>
             <p>비밀번호 재확인 </p>
             <input
               type="password"
-              value={passwordCheck || ""}
+              value={passwordCheck || ''}
               onChange={onChangePasswordCheck}
               placeholder="비밀번호를 확인 주세요"
             />
-            <label warning={"default"}>
+            <label warning={'default'}>
               {/* <span warning={passwordDoubleCheck ? false : true}> */}
               동일한 비밀번호를 입력하세요
             </label>
+            <button
+              className={styles.signupBtn}
+              onClick={() => {
+                setView(!view);
+              }}
+            >
+              <img src={view ? Icon_G_EyeOpen : Icon_G_EyeClose} />
+            </button>
           </div>
         </div>
 
         <div className={styles.buttonBox}>
-          <button onClick={handleClickSignup}>가입하기</button>
+          <button onClick={handleClickSignup}>ToonDa 가입하기</button>
           <span onClick={goLogin}>로그인 하러 가기</span>
         </div>
       </div>
