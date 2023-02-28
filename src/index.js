f1844b294bede212e1a68ef248da67272fc08c3a
thdud <thgml1444@naver.com>
2023-02-28 17:59:22 +0900

    [ADD] react-query 라이브러리를 설치하라.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/global.module.scss";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/global.module.scss';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+      </React.StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
